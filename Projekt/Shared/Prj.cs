3c8e64ff02c3a1c1fa75985d773b7d7bf4bdcb80
Dragan Pepic <d.pepic@hotmail.com>
2020-10-20 18:01:20 +0200

    Add project files.

---
+++ Projekt/Shared/Prj.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt.Shared
+{
+	public class Prj
+	{
+		public string Bla { get; set; }
+		public string BlaBla { get; set; }
+	}
+}
